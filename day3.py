2b80cd5d3bc3d71437be02d2750351ece5bcf341
anaghabhat31370 <anaghabhat31370@gmail.com>
2026-08-28 20:20:20 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,7 @@
+# Day 3 
+
+Today I continued learning Python and built on the concepts from my previous practice.
+
+# Practice
+
+I practiced writing a Python program that takes information from the user and performs calculations with it.
