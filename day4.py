39076d777f15341a36b7e83244f1adde27d6226b
anaghabhat31370 <anaghabhat31370@gmail.com>
2026-08-29 21:40:36 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,22 @@
+print("Day 4 - Python Learning")
+
+print("Today I learned about variables.")
+print("I learned how to store information in variables.")
+print("I learned how to print variables.")
+print("I also learned how to use variables in calculations.")
+
+name = "Anagha"
+age = 17
+
+print("My name is", name)
+print("I am", age, "years old")
+
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+print("Today I understood how variables make programs easier to use.")
